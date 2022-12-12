9dbfb73d0ecbdce7b647f62d023dbbb5f5a9de29
Pamidonte <francescomarrone8@gmail.com>
2022-12-12 18:01:25 +0100

    Aggiungere i file di progetto.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+     class Indirizzo
+    {
+        string name;
+        string surname;
+        string street;
+        string city;
+        string province;
+        string zip;
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+        public void stampaIndirizzo() {
+        Console.WriteLine("nome: " + this.name );
+            Console.WriteLine("surname: " + this.surname);
+            Console.WriteLine("street: " + this.street);
+            Console.WriteLine("city: " + this.city);
+            Console.WriteLine("province: " + this.province);
+            Console.WriteLine("zip: " + this.zip);
+
+
+        }
+
+    }
+
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic.FileIO;
+namespace ListaIndirizzi { 
+class Program{
+        public static void Main(string[] args) {
+            string pathFile = "C:\\Users\\franc\\source\\repos\\ListaIndirizziFile.txt";
+            string[] lines = File.ReadAllLines(pathFile);
+            List<Indirizzo> result = new List<Indirizzo>();
+            foreach (string line in lines)
+            {
+                string[] data = line.Split(",");
+                if(data.Length != 6 || data.Any(" ".Contains)) {
+                  Console.WriteLine("attenzione ci sono degli errori in: " + line);
+
+                }
+                else
+                {
+                    Indirizzo indirizzo = new Indirizzo(data[0], data[1], data[2], data[3], data[4], data[5]);
+                    result.Add(indirizzo);
+                }
+              
+            }
+            foreach (Indirizzo item in result)
+            {
+                item.stampaIndirizzo();
+
+            }
+
+
+
+
+
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+}    
